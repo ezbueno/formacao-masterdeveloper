d78cea74858f6ad8eff579f27a34e64d3bf3b57b
Ezandro Bueno <57026708+ezbueno@users.noreply.github.com>
2020-08-25 02:51:58 -0300

    Desenvolvimento do jogo da velha

---
+++ masterdeveloper-tictactoe/src/main/java/br/com/softblue/tictactoe/Main.java
@@ -0,0 +1,15 @@
+package br.com.softblue.tictactoe;
+
+import java.io.IOException;
+
+import br.com.softblue.tictactoe.core.Game;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		Game game = new Game();
+		game.play();
+		
+	}
+}
