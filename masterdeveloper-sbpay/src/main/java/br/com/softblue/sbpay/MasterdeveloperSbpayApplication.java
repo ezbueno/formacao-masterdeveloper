7f51f2eb59235ba5fbc0176baf470158096474a7
Ezandro <ezandrobueno@yahoo.com.br>
2020-08-25 02:37:17 -0300

    Criação do Web service de pagamento da aplicação Bluefood

---
+++ masterdeveloper-sbpay/src/main/java/br/com/softblue/sbpay/MasterdeveloperSbpayApplication.java
@@ -0,0 +1,13 @@
+package br.com.softblue.sbpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterdeveloperSbpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterdeveloperSbpayApplication.class, args);
+	}
+
+}
---
+++ masterdeveloper-sbpay/src/test/java/br/com/softblue/sbpay/MasterdeveloperSbpayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.softblue.sbpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterdeveloperSbpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
