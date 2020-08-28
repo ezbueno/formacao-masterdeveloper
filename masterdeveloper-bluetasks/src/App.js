e73c246e16ba87c09c01045b93c74169e0ed53b8
Ezandro Bueno <ezandrobueno@yahoo.com.br>
2020-08-28 02:33:52 -0300

    BlueTasks
    Implementação front-end das funcionalidades da aplicação BlueTasks usando o React e Bootstrap

---
+++ masterdeveloper-bluetasks/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import NavBar from './Components/NavBar';
+
+class App extends Component {
+  //constructor(props) {
+    //super(props);
+  //}
+  render() {
+    return (
+      <div className="App">
+        <NavBar/>
+      </div>
+    );
+  }
+}
+
+export default App;
