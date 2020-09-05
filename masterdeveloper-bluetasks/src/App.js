48c34a1a850ac5cd0936569ca9b6f88656076761
Ezandro Bueno <57026708+ezbueno@users.noreply.github.com>
2020-09-05 02:50:51 -0300

    Implementação front-end da aplicação BlueTasks

--- masterdeveloper-bluetasks/src/App.js
+++ masterdeveloper-bluetasks/src/App.js
@@ -3,6 +3,7 @@ import NavBar from './Components/NavBar';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import TaskListTable from './Components/TaskListTable';
 import TaskForm from './Components/TaskForm';
+import Login from './Components/Login';
 
 class App extends Component {
   //constructor(props) {
@@ -15,6 +16,7 @@ class App extends Component {
           <NavBar/>
             <div className="container" style={{marginTop: 20}}>
               <Switch>
+                <Route exact path="/login" component={Login}/>
                 <Route exact path="/form" component={TaskForm}/>
                 <Route exact path="/form/:id" component={TaskForm}/>
                 <Route path="/" component={TaskListTable}/>
---
+++ masterdeveloper-bluetasks/src/api/AuthService.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import {AUTH_ENDPOINT, JWT_TOKEN_NAME} from "../constants"
+
+class AuthService {
+    login(username, password, onLogin) {
+        console.log(AUTH_ENDPOINT);
+        axios
+            .post(`${AUTH_ENDPOINT}/login`, {username: username, password: password})
+            .then(response => {
+                const jwtToken = response.headers['authorization'].replace("Bearer ", "");
+                sessionStorage.setItem(JWT_TOKEN_NAME, jwtToken);
+                onLogin(true);
+            }).catch(error => {
+                console.error(error);
+                onLogin(false);
+            });
+    }
+}
+
+export default new AuthService();
