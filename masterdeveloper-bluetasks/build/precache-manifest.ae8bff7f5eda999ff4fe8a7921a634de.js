4ac10308cbb455e021027466ce1154d93b1d8f83
Ezandro Bueno <57026708+ezbueno@users.noreply.github.com>
2020-09-10 16:27:58 -0300

    Implementação front-end da aplicação BlueTasks

---
+++ masterdeveloper-bluetasks/build/precache-manifest.ae8bff7f5eda999ff4fe8a7921a634de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "042ba776128e5a1076538c6ace8f69d9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bb8ded4c09b85da8c650",
+    "url": "./static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "bb8ded4c09b85da8c650",
+    "url": "./static/js/2.d20bb18a.chunk.js"
+  },
+  {
+    "revision": "306f3bf9d2f1ee8399ad560a7731d0d6",
+    "url": "./static/js/2.d20bb18a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a3cf4da0c1c5bde5037",
+    "url": "./static/js/main.c707312f.chunk.js"
+  },
+  {
+    "revision": "2b6e286a7d9b8c466aec",
+    "url": "./static/js/runtime-main.1e4e7897.js"
+  }
+]);
