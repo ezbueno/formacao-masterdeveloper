06d983e3973850c79a6ece3242a9190e55478cb3
Ezandro Bueno <57026708+ezbueno@users.noreply.github.com>
2020-09-10 16:38:52 -0300

    Delete DuplicateTaskException.java

--- masterdeveloper-bluetasks-backend/masterdeveloper-bluetasks-backend/src/main/java/br/com/softblue/bluetasks/domain/task/DuplicateTaskException.java
+++
@@ -1,9 +0,0 @@
-package br.com.softblue.bluetasks.domain.task;
-
-@SuppressWarnings("serial")
-public class DuplicateTaskException extends Exception {
-
-	public DuplicateTaskException(String message) {
-		super(message);
-	}
-}
